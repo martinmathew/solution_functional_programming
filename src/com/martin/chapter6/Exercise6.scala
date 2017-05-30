08d18c0f0fe947a56108358d0f178ab99eb8d3b1
martinmathew <martin.mathew.2k11@gmail.com>
2017-05-30 06:44:02 -0500

    ex 6

--- src/com/martin/chapter6/Exercise6.scala
+++ src/com/martin/chapter6/Exercise6.scala
@@ -2,6 +2,8 @@ package com.martin.chapter6
 
 import java.lang.Math.RandomNumberGeneratorHolder
 
+import com.martin.chapter6.Exercise6.unit
+
 /**
   * Created by martin on 4/29/17.
   */
@@ -127,8 +129,6 @@ a new action that combines them:
     (f(a,b),brng)
   }
 
-
-
   def map[A,B](s:Rand[A])(f:A=>B):Rand[B] = rng => {
     val (a,rng2) = s(rng)
     (f(a),rng2)
@@ -154,7 +154,6 @@ repeated n times.
     ( res.map(_._1),res(res.size-1)._2)
   }
 
-
   def sequence1[A](fs:List[Rand[A]]):Rand[List[A]] = {
     fs.foldLeft(unit(Nil: List[A]))(( acc,el) => map2(el, acc)(_ :: _))
   }
@@ -177,4 +176,97 @@ def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B]
   def mapViaFlatmap[A,B](s:Rand[A])(f:A=>B):Rand[B] =  {
     flatMap(s)(a=>unit(f(a)))
   }
+
+  /**
+    * EXERCISE 6.9
+Reimplement map and map2 in terms of flatMap . The fact that this is possible is what
+we’re referring to when we say that flatMap is more powerful than map and map2 .
+    *
+    *
+    *
+    * @param ra
+    * @param rb
+    * @param f
+    * @tparam A
+    * @tparam B
+    * @tparam C
+    * @return
+    */
+  def map2ViaFlatMap[A,B,C](ra:Rand[A],rb:Rand[B])(f:(A,B)=>C):Rand[C] =
+   rng => {  flatMap((ra))(a=>flatMap(rb)(b=>unit(f(a,b))))}
+
+  /**
+    *EXERCISE 6.10
+Generalize the functions unit , map , map2 , flatMap , and sequence . Add them as meth-
+ods on the State case class where possible. Otherwise you should put them in a State
+companion object.
+    *
+    * @param run
+    * @tparam S
+    * @tparam A
+    */
+  case class State[S,+A](run:S=>(A,S)){
+  def map[B](f:A =>B):State[S,B] = State(s => {
+    val (a,s1)=run(s)
+    (f(a),s1)
+  })
+
+  def flatMap[B](f:A=>State[S,B]):State[S,B] = State(s => {
+    val (a,s1) = run(s)
+    f(a).run(s1)
+  })
+
+  def map2[B,C](rb:State[S,B])(f:(A,B) => C):State[S,C] =
+       flatMap(a=>rb.map(b => f(a,b)))
+
+}
+
+
+  object State {
+    def unit[S,A](a:A):State[S,A] = State(rng => (a,rng))
+
+    def sequence1[S,A](fs:List[State[S,A]]):State[S,List[A]] =
+      fs.foldLeft(unit[S,List[A]](Nil))(( acc,el) => el.map2(acc)(_ :: _))
+
+
+  }
+
+
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+
+
+
+  case class Machine(locked:Boolean,candies:Int,coins:Int){
+    def simulatemachine(inputs:List[Input]):State[Machine,(Int,Int)] = {
+      (inputs.headOption,inputs.tail) match {
+        case (Some(inp), List.empty) => {
+          val mach = this.fun(inp)
+          State.unit[Machine,(Int,Int)](mach.candies,mach.coins)
+        }
+        case (Some(inp),x) => this.fun(inp).simulatemachine(x)
+      }
+    }
+
+
+
+    private def fun(inp:Input):Machine = {
+      inp match {
+        case Coin => if(this.locked) Machine(false,this.candies,this.coins+1) else this
+        case Turn => if (this.locked) this else Machine(true,this.candies-1,this.coins)
+      }
+    }
+
+  }
+
+
+
+object MachineOps{
+
+
+}
+
+
+
 }
